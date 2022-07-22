2dfca2a170988990d24c3897cc6b27dc2b83a046
martinni87 <30123957+martinni87@users.noreply.github.com>
2022-07-22 23:59:17 +0200

    Dicee exercise done

--- 03_Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ 03_Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceOne: UIImageView!
+    @IBOutlet weak var diceTwo: UIImageView!
+    @IBOutlet weak var diceThree: UIImageView!
+    @IBOutlet weak var rollingButton: UIButton!
+    
+    let diceValuesArray = [
+        UIImage(named: "DiceOne"),
+        UIImage(named: "DiceTwo"),
+        UIImage(named: "DiceThree"),
+        UIImage(named: "DiceFour"),
+        UIImage(named: "DiceFive"),
+        UIImage(named: "DiceSix")
+    ]
+
+//    In this case we don't really need vewDidLoad() function, so I just comment it.
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        Next commented lines are just part of the exercise.
+//        diceOne.image = UIImage(named: "DiceOne")
+//        diceTwo.image = UIImage(named: "DiceTwo")
+//        diceThree.image = UIImage(named: "DiceThree")
+//        rollingButton.alpha = CGFloat(0.5)
+//    }
+
+    @IBAction func rollingButtonAction(_ sender: UIButton) {
+        //Next commented lines are just part of the exercise.
+//        print("Button has been pressed")
+//        diceOne.image = .init(named: "DiceFour")
+//        diceTwo.image = .init(named: "DiceFive")
+//        diceThree.image = .init(named: "DiceSix")
+//        rollingButton.alpha = CGFloat(1)
+        diceOne.image = diceValuesArray[Int.random(in: 0...5)]
+        //Other way is with .randomElement() which looks how many items we have in an array and
+        //randomly choose one. This is way more efficient.
+        //If we don't set .randomElement()! we get an error: Cannot assign value of type UIImage?? to type
+        //UIImage?
+        //With ! we confirm that this value is not going to be null.
+        diceTwo.image = diceValuesArray.randomElement()!
+        diceThree.image = diceValuesArray.randomElement()!
     }
-
-
+    
+    
 }
 
