a5e9b963cc81919aa4daa678c29e167ebd7bb2f8
martinni87 <30123957+martinni87@users.noreply.github.com>
2022-07-25 19:18:23 +0200

    Calculator UI done and responsive to all sizes

--- 05b_AutoLayoutProject2/Calculator-Layout-iOS13/Calculator Layout iOS13/ViewController.swift
+++ 05b_AutoLayoutProject2/Calculator-Layout-iOS13/Calculator Layout iOS13/ViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let numPadValue = [0,1,2,3,4,5,6,7,8,9]
+    
+    enum operationToDo{
+        case addition
+        case subtraction
+        case multiplication
+        case division
+        case percentage
+        case togglePositiveNegative
+        case allClear
+    }
+    
+    @IBOutlet weak var resultDisplay: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
