e7deac8de43391cde095216abfb1cb3915372cab
martinni87 <30123957+martinni87@users.noreply.github.com>
2022-08-27 18:06:49 +0200

    Created new project 10 version 2 to practice the use of segues

---
+++ 10_BMI_Calculator_v2/BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var heightValueLabel: UILabel!
+    @IBOutlet weak var weightValueLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var calculateButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func heightSliderAction(_ sender: UISlider) {
+        heightValueLabel.text = String(format: "%.2f", sender.value) + "m"
+    }
+    
+    @IBAction func weightSliderAction(_ sender: UISlider) {
+        weightValueLabel.text = String(format: "%.0f", sender.value) + "kg"
+    }
+    
+    @IBAction func calulateButtonAction(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight/pow(height,2)
+
+        let resultVC = ResultViewController()
+        
+        resultVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(resultVC, animated: true, completion: nil)
+    }
+}
+
